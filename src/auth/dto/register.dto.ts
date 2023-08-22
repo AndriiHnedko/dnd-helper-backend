3c5d2b2019969578b0f40fd553fe7f6846575b62
Andrii Hnedko <andrii.hnedko@gmail.com>
2023-08-23 00:07:17 +0300

    update register dto validation

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+import { Unique } from '../../validators';
 
 export class RegisterDto {
   @IsEmail()
   @IsNotEmpty()
   @MinLength(3)
   @ApiProperty()
+  @Unique('user', 'email')
   email: string;
 
   @IsString()
