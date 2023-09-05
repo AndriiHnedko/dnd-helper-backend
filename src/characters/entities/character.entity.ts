6b39c62f65777f6cb43369b541fa5d3b23d66f0a
Andrii Hnedko <andrii.hnedko@gmail.com>
2023-09-05 20:31:13 +0300

    add character class transformer to CharacterEntity

--- src/characters/entities/character.entity.ts
+++ src/characters/entities/character.entity.ts
@@ -1,14 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Character } from '@prisma/client';
 import { Exclude } from 'class-transformer';
+import { CharacterClassEntity } from '../../character-class/entities/character-class.entity';
 import { UserEntity } from '../../users/entities/user.entity';
 
 export class CharacterEntity implements Character {
-  constructor({ user, ...data }: Partial<CharacterEntity>) {
+  constructor({ user, characterClass, ...data }: Partial<CharacterEntity>) {
     Object.assign(this, data);
 
     if (user) {
       this.user = new UserEntity(user);
+    }
+    if (characterClass) {
+      this.characterClass = new CharacterClassEntity(characterClass);
     }
   }
 
@@ -23,7 +27,6 @@ export class CharacterEntity implements Character {
 
   @Exclude()
   classId: number;
-  //todo class
 
   @Exclude()
   subClassId: number;
@@ -34,4 +37,7 @@ export class CharacterEntity implements Character {
 
   @ApiProperty({ type: UserEntity })
   user: UserEntity;
+
+  @ApiProperty({ type: CharacterClassEntity })
+  characterClass: CharacterClassEntity;
 }
